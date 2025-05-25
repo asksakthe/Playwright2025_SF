51634dbe69f982d7837c85a16da9650e3e948edc
asksakthe <asksakthe@outlook.com>
2025-05-25 15:47:26 +0530

    first commit PW SF practise

---
+++ tests/launchHome.spec.ts
@@ -0,0 +1,18 @@
+import {expect, test} from "@playwright/test"
+
+test("launch a browser with SF", async ({page}) => {
+    await page.goto("https://orgfarm-3fe5b17d48-dev-ed.develop.my.salesforce.com/")
+    await page.locator("#username").fill("asksakthe409@agentforce.com")
+    await page.locator("#password").fill("Sakthi@123")
+    await page.locator("#Login").click()
+    await page.waitForTimeout(5000)
+    //title
+    await expect(page).toHaveTitle("Home | Salesforce")
+
+    await page.locator("//span[text()='App Launcher']").click()
+    await page.getByPlaceholder("Search apps and items...").fill("accounts")
+    await page.locator("//p/b[text()='Accounts']").click()
+    //await page.locator("//p[text()='Marketing CRM Classic']").click()
+    await page.waitForTimeout(10000)
+    
+})
